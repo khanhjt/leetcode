cc15227d84192f5b4702b2fc9b61ac6c2f970d88
khanhjt <105477211+khanhjt@users.noreply.github.com>
2024-02-27 04:59:37 +0000

    Update user authentication logic

---
+++ remove_element.py
@@ -0,0 +1,5 @@
+#  nums[int] = [3, 2, 2, 3], val = 3
+# nums[int] = [0,2,2,0]
+# cho một mảng số nguyên nums, và 1 số nguyên val. Xóa tất cả các phần tử có giá trị val trong nums.
+# Trả về số lượng phần tử sau khi xóa.
+# Không cần xóa vật lý các phần tử.
