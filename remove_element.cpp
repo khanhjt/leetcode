36513f3ad99268d3852acc9602c3875933aa0053
Nguyen Huu Khanh <105477211+khanhjt@users.noreply.github.com>
2024-05-14 09:34:21 +0700

    Update remove_element.cpp

--- remove_element.cpp
+++ remove_element.cpp
@@ -1,16 +1,14 @@
-#  nums[int] = [3, 2, 2, 3], val = 3
-# nums[int] = [0,2,2,0]
-# cho một mảng số nguyên nums, và 1 số nguyên val. Xóa tất cả các phần tử có giá trị val trong nums.
-# Trả về số lượng phần tử sau khi xóa.
-# Không cần xóa vật lý các phần tử.
+// val là 1 số, nếu trong mảng có val thì xóa
+// return mảng nums sau khi xóa, và size
 class Solution {
 public:
-      int removeElement(vector<int>& nums, int val){
+    int removeElement(vector<int>& nums, int val) {
         vector<int> a;
-        int count = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums(i) != val){
-              a.push_back(nums(i));
-              i +=1;}}
-        nums = a;
-        return i;};
+            if(nums[i] != val){
+                a.push_back(nums[i]);
+            }}
+            nums = a;
+            return a.size();
+    }
+};
